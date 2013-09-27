92a4131fdfd1e38d997d1a016fd786aaf9e35ad5
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-09-26 22:10:48 -0400

    updated to use Renderable

--- src/guestbook/views/layout.clj
+++ src/guestbook/views/layout.clj
@@ -1,9 +1,18 @@
 (ns guestbook.views.layout
   (:use noir.request)
-  (:require [selmer.parser :as parser]))
+  (:require [selmer.parser :as parser]
+    [ring.util.response :refer [content-type response]]))
 
 (def template-path "guestbook/views/templates/")
 
+(deftype RenderableTemplate [template params]
+  Renderable
+  (render [this request]
+    (content-type
+      (->> (assoc params :servlet-context (:context request))
+        (parser/render-file (str template-path template))
+        response)
+      "text/html; charset=utf-8")))
+
 (defn render [template & [params]]
-  (parser/render-file (str template-path template)
-                      (assoc params :context (:context *request*))))
+  (RenderableTemplate. template params))
