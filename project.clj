2389b14f9450050ddbe44580f7728ac59ba81c5c
Yogthos <dmitri.sotnikov@gmail.com>
2014-04-28 09:02:12 -0400

    updated guestbook to put templates under resources

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject
-  guestbook
-  "0.1.0-SNAPSHOT"
+  guestbook "0.1.0-SNAPSHOT"
+  :url "https://github.com/yogthos/guestbook"
+  :description "sample guestbook application"
   :dependencies
   [[com.h2database/h2 "1.3.174"]
    [ring-server "0.3.1"]
@@ -21,23 +22,22 @@
    [compojure "1.1.6"]
    [lib-noir "0.7.9"]
    [com.postspectacular/rotor "0.1.0"]]
+
+  :plugins
+  [[lein-ring "0.8.7"] [lein-environ "0.4.0"]]
+
   :ring
   {:handler guestbook.handler/app,
    :init guestbook.handler/init,
    :destroy guestbook.handler/destroy}
+
   :profiles
-  {:production
+  {:uberjar {:aot :all}
+   :production
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}},
    :dev
    {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]],
     :env {:selmer-dev true}}}
-  :url
-  "http://example.com/FIXME"
-  :aot
-  :all
-  :plugins
-  [[lein-ring "0.8.7"] [lein-environ "0.4.0"]]
-  :description
-  "FIXME: write description"
+
   :min-lein-version "2.0.0")
