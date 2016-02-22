0198c702d0c544155527405e9d272983e0d0c9d3
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-21 20:03:20 -0500

    added working test

--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -1,32 +1,25 @@
 (ns guestbook.test.db.core
-  (:require [guestbook.db.core :as db]
+  (:require [guestbook.db.core :refer [*db*] :as db]
             [guestbook.db.migrations :as migrations]
             [clojure.test :refer :all]
             [clojure.java.jdbc :as jdbc]
             [config.core :refer [env]]
+            [conman.core :refer [with-transaction]]
             [mount.core :as mount]))
 
 (use-fixtures
   :once
   (fn [f]
+    (mount/start #'guestbook.db.core/*db*)
     (migrations/migrate ["migrate"])
     (f)))
 
 (deftest test-users
-  (jdbc/with-db-transaction [t-conn db/conn]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"} {:connection t-conn})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user {:id "1"} {:connection t-conn})))))
+  (jdbc/with-db-transaction [t-conn *db*]
+      (jdbc/db-set-rollback-only! t-conn)
+      (let [message {:name "test"
+                     :message "test"
+                     :timestamp (java.util.Date.)}]
+        (is (= 1 (db/save-message! t-conn message)))
+        (is (= [(assoc message :id 1)] (db/get-messages t-conn {})))))
+  (is (empty? (db/get-messages))))
