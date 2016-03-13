f779c0294dabff0700f6ecce0b4710470d680cb9
Yogthos <dmitri.sotnikov@gmail.com>
2016-03-13 10:11:50 -0400

    updated sample test

--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -18,19 +18,12 @@
 (deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (let [message {:name "test"
+                   :message "test"
+                   :timestamp (java.util.Date.)}]
+      (is (= 1 (db/save-message! t-conn message)))
+      (let [result (db/get-messages t-conn {})]
+        (is (= 1 (count result)))
+        (is (= message (dissoc (first result) :id))))))
+  (is (empty? (db/get-messages))))
+
